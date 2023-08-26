b5ae074fd991c74c8c7fbf1edae8cd4de57799bb
ahmadhuss <ahmadhussnain787@gmail.com>
2023-08-27 00:57:53 +0500

    v1.0.1

--- stamp.js
+++ stamp.js
@@ -4,39 +4,43 @@ if (!title) {
     $('body').text('No title provided.')
 }
 
-$(document).ready(function () {
+$(document).ready(async function () {
     console.log('ready');
     $('#stamp-name').text(title);
     new CircleType(document.getElementById('stamp-name'));
-    $(window).on('load', async function () {
-        console.log('load');
-        if (title) {
-            let element = $(".image-stamp"); // global variable
-            console.log(element);
-            element.width(element.height());
-            $('#stamp-name div').css('transform', 'rotate(90deg)');
 
-            try {
-                const canvas = await html2canvas(element);
-                const dataURI = canvas.toDataURL("image/png");
-                console.log(dataURI);
-                // Form Submission
-                const qr_form_ele = $('#qr_form');
-                const qr_code_ele = $('#qr_code');
-                qr_code_ele.val(dataURI);
-                // qr_form_ele.submit();
+    if (title) {
+        let element = $(".image-stamp"); // global variable
+        console.log(element);
+        element.width(element.height());
+        $('#stamp-name div').css('transform', 'rotate(90deg)');
+        console.log('title:', title);
 
-
-                const div = $(`<div id="box">`);
-                div.text(dataURI);
-                div.appendTo('body');
+        try {
+            const canvas = await html2canvas(element);
+            const dataURI = canvas.toDataURL("image/png");
+            console.log(dataURI);
+            // Form Submission
+            const qr_form_ele = $('#qr_form');
+            const qr_code_ele = $('#qr_code');
+            qr_code_ele.val(dataURI);
+            // qr_form_ele.submit();
 
 
-            } catch (error) {
-                console.error('Error during html2canvas:', error);
-            }
-        } else {
-            $('body').text('No title provided.')
+            const div = $(`<div id="box">`);
+            div.text(dataURI);
+            div.appendTo('body');
+
+
+        } catch (error) {
+            console.error('Error during html2canvas:', error);
         }
+    } else {
+        $('body').text('No title provided.')
+    }
+
+
+    $(window).on('load', async function () {
+        console.log('load will not run on selenium and puppeter');
     });
 });
